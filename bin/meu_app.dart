0afeb0a77fe4a4fd364a2d7836c6a1c5b2ef0230
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-02 17:31:08 -0300

    fazendo lista de tarefas  3 - final

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
-void main(){
-  print("Olá, Qual é seu nome?");
-  var nome = stdin.readLineSync();
-  print("oie $nome. Prazer em conhecer você. Qual é sua idade?");
-  var idade = stdin.readLineSync();
-  print("entao voce é chama $nome e tem $idade");
+void main() {
+  print("Olá, me chamo Dart. Qual o seu nome?");
+  String? nome = stdin.readLineSync();
+  print("Muito prazer, $nome. Vamos fazer vários programas juntos.");
 }
