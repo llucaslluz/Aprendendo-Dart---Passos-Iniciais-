89227703a7314b378379e7fd7def4e2545d9f35c
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-02 17:22:03 -0300

    fazendo lista de tarefas  1

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
 void main() {
-  print("Hello, World!");
+  print("Olá, me chamo Dart. Qual o seu nome?");
+  var nome = stdin.readLineSync();
+  print("Muito prazer, $nome. Vamos fazer vários programas juntos.");
 }
